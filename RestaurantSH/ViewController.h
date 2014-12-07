84c962584639b11cbe212e9d85bdf6f8c950bcd3
kzyn <yanokazuto@KYsMBA.local>
2014-12-07 17:44:21 +0900

    Initial Commit

---
+++ RestaurantSH/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RestaurantSH
+//
+//  Created by kzyn on 2014/12/07.
+//  Copyright (c) 2014年 wcjp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
